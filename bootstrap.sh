8cd08d40b72cf3d6817b5f8da3d25e2559c02eb9
Vick Mena <vickmena@gmail.com>
2021-03-23 11:20:52 -0400

    Install & Test git-extension-pack

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-yum -y install -y epel-release
-yum -y install -y wget curl xorg-x11-xauth xterm lsof strace socat nc tree tcpdump vim jq git cmake3
+yum -y install -y epel-release centos-release-scl
+yum -y install -y wget curl xorg-x11-xauth xterm lsof strace socat nc tree tcpdump vim jq git cmake3 rh-git218
 #yum -y install https://github.com/VSCodium/vscodium/releases/download/1.53.2/codium-1.53.2-1613090035.el7.x86_64.rpm
 #yum -y install https://github.com/VSCodium/vscodium/releases/download/1.52.1/codium-1.52.1-1608165610.el7.x86_64.rpm
 #yum -y install https://github.com/VSCodium/vscodium/releases/download/1.51.1/codium-1.51.1-1605141277.el7.x86_64.rpm
@@ -49,6 +49,7 @@ sudo -u vagrant cp -v /vagrant/defaults.json .config/VSCodium/User/settings.json
 sudo -u vagrant wget -q https://open-vsx.org/api/vscodevim/vim/1.18.9/file/vscodevim.vim-1.18.9.vsix
 sudo -u vagrant wget -q https://open-vsx.org/api/ccls-project/ccls/0.1.29/file/ccls-project.ccls-0.1.29.vsix
 sudo -u vagrant wget -q https://open-vsx.org/api/redhat/java/0.76.0/file/redhat.java-0.76.0.vsix
+sudo -u vagrant wget -q https://open-vsx.org/api/donjayamanne/git-extension-pack/0.1.3/file/donjayamanne.git-extension-pack-0.1.3.vsix
 
 # Not supported in 1.48.2
 #sudo -u vagrant wget -q https://open-vsx.org/api/eamodio/gitlens/11.3.0/file/eamodio.gitlens-11.3.0.vsix
@@ -59,4 +60,7 @@ done
 
 # Get a sample
 sudo -u vagrant git clone https://github.com/ttroy50/cmake-examples.git ~vagrant/examples
+
+# Enable git-2.18 for vagrant
+echo "source /opt/rh/rh-git218/enable" >> ~vagrant/.bashrc
 
