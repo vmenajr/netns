156dff70ec1f5b441bd15ebd8eede9313d86201a
Vick Mena <vickmena@gmail.com>
2021-03-23 10:28:24 -0400

    Include additional plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -48,6 +48,9 @@ sudo -u vagrant cp -v /vagrant/defaults.json .config/VSCodium/User/settings.json
 # Install the plugins
 sudo -u vagrant wget -q https://open-vsx.org/api/vscodevim/vim/1.18.9/file/vscodevim.vim-1.18.9.vsix
 sudo -u vagrant wget -q https://open-vsx.org/api/ccls-project/ccls/0.1.29/file/ccls-project.ccls-0.1.29.vsix
+sudo -u vagrant wget -q https://open-vsx.org/api/redhat/java/0.76.0/file/redhat.java-0.76.0.vsix
+sudo -u vagrant wget -q https://open-vsx.org/api/eamodio/gitlens/11.3.0/file/eamodio.gitlens-11.3.0.vsix
+
 for f in *.vsix; do
     sudo -u vagrant codium --install-extension $f
 done
